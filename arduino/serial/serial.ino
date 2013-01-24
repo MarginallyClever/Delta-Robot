4b31acf0b6357367b66baca4358c526d2bd3c1bf
Dan Royer <dan@marginallyclever.com>
2013-01-24 12:44:36 -0800

    3
    Added serial.ino
    Moved arduino.ino to sinecurve.ino
    changed pins to allow 4-armed delta robots

---
+++ arduino/serial/serial.ino
@@ -0,0 +1,203 @@
+//------------------------------------------------------------------------------
+// Delta robot simple communication test
+// dan@marginallycelver.com 2013 jan 23
+//------------------------------------------------------------------------------
+// Copyright at end of file.
+// please see http://www.github.com/i-make-robots/DeltaRobot for more information.
+
+
+//------------------------------------------------------------------------------
+// constants
+//------------------------------------------------------------------------------
+const int MAX_ANGLE    = 90+12;
+const int MIN_ANGLE    = 90-35;
+const int NUM_ARMS     = 3;  // can be 3 or 4.
+
+// Serial communication bitrate
+const long BAUD        = 57600;
+// Maximum length of serial input message.
+const int MAX_BUF      = 64;
+
+
+//------------------------------------------------------------------------------
+// includes
+//------------------------------------------------------------------------------
+#include <Servo.h>
+
+
+//------------------------------------------------------------------------------
+// structures
+//------------------------------------------------------------------------------
+struct Arm {
+  Servo s;
+  float a;
+};
+
+
+//------------------------------------------------------------------------------
+// globals
+//------------------------------------------------------------------------------
+Arm arms[NUM_ARMS];
+
+
+// Serial comm reception
+static char buffer[MAX_BUF];  // Serial buffer
+static int sofar;             // Serial buffer progress
+
+
+//------------------------------------------------------------------------------
+// methods
+//------------------------------------------------------------------------------
+
+/**
+ * sets a servo to a given angle.
+ * @param angle [0...180].
+ */
+void moveArm(int id,float angle) {
+  if(angle>MAX_ANGLE) {
+    Serial.println(F("MAX_ANGLE"));
+    angle = MAX_ANGLE;
+  }
+  if(angle<MIN_ANGLE) {
+    Serial.println(F("MIN_ANGLE"));
+    angle = MIN_ANGLE;
+  }
+  Serial.print(id);
+  Serial.print(F("="));
+  Serial.println(angle);
+  
+  arms[id].a=angle;
+  // convert range 180-0 to 2000-1000
+  arms[id].s.writeMicroseconds((int)( (angle * 1000.0/180.0 ) + 1000.0));
+}
+
+
+/**
+ * write the position of each arm to the serial output
+ */
+void printState() {
+  int i;
+  for(i=0;i<NUM_ARMS;++i) {
+    if(i>0) Serial.print(F("\t"));
+    Serial.print(arms[i].a);
+  }
+  Serial.print(F("\n"));
+}
+
+
+/**
+ * interpret commands sent to the robot
+ */
+void processCommand() {
+  if(buffer[0]==';') return;
+  
+  char *ptr=buffer;
+  while(*ptr && ptr<buffer+sofar ) {
+//    Serial.println(ptr);
+    
+    char letter=*ptr;
+    float number=atof(ptr+1);
+    
+    int id = letter-'A';
+/*
+    Serial.print(F("letter="));
+    Serial.println(letter);
+    Serial.print(F("number="));
+    Serial.println(number);
+    Serial.print(F("id    ="));
+    Serial.println(id,DEC);
+*/
+    if(id<0 || id>=NUM_ARMS) {
+      Serial.println(F("Invalid id."));
+    } else {
+      moveArm(id,number);
+    }
+    ptr=strchr(ptr,' ')+1;
+  }
+}
+
+
+/**
+ * listen for instructions coming from the serial connection.
+ * See: http://www.marginallyclever.com/2011/10/controlling-your-arduino-through-the-serial-monitor/
+ */
+void listenToSerial() {
+  while(Serial.available() > 0) {
+    buffer[sofar++]=Serial.read();
+    if(buffer[sofar-1]==';') break;  // in case there are multiple instructions
+  }
+ 
+  // if we hit a semi-colon, assume end of instruction.
+  if(sofar>0 && buffer[sofar-1]==';') {
+    buffer[sofar]=0;
+    
+    // echo confirmation
+    Serial.println(buffer);
+ 
+    // do something with the command
+    processCommand();
+ 
+    // reset the buffer
+    sofar=0;
+ 
+    // echo completion
+    Serial.print(F("> "));
+  }
+}
+
+
+/**
+ * runs once when board turns on/resets.  Initializes variables.
+ */
+void setup() {
+  // initialize the read buffer
+  sofar=0;
+  // start serial communications
+  Serial.begin(BAUD);
+  Serial.println(F("** START **"));
+  
+  int i;
+  for(i=0;i<NUM_ARMS;++i) {
+    // connect to the servos
+    arms[i].s.attach(5-i);
+    // center the arm
+    moveArm(i,90);
+  }
+  
+  printState();
+  
+  Serial.print(F("> "));
+}
+
+
+/**
+ * runs over and over.  if loop() finishes then the board calls it again. globals do not get reset if loop() ends.
+ */
+void loop() {
+  listenToSerial();
+  delay(20);
+  //printState();
+}
+
+
+//------------------------------------------------------------------------------
+// Copyright (C) 2012 Dan Royer (dan@marginallyclever.com)
+// Permission is hereby granted, free of charge, to any person obtaining a 
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation 
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the 
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//------------------------------------------------------------------------------
+
--- arduino/sinecurve/sinecurve.ino
+++ arduino/sinecurve/sinecurve.ino
@@ -7,33 +7,47 @@
 
 
 //------------------------------------------------------------------------------
-const int MAX_ANGLE = 10;
-const int MIN_ANGLE = -20;
+// constants
+//------------------------------------------------------------------------------
+const int MAX_ANGLE = 12;
+const int MIN_ANGLE = -35;
 const int DIF_ANGLE = MAX_ANGLE+MIN_ANGLE/2;
 const int TOTAL_ANGLE = MAX_ANGLE-MIN_ANGLE;
 
 
 //------------------------------------------------------------------------------
+// includes
+//------------------------------------------------------------------------------
 #include <Servo.h>
 
 
+//------------------------------------------------------------------------------
+// globals
 //------------------------------------------------------------------------------
 Servo s000,s120,s240;
 
 
 //------------------------------------------------------------------------------
+// setup
+// runs once when board turns on/resets.  Initializes variables.
+//------------------------------------------------------------------------------
 void setup() {
+  // start serial communications
   Serial.begin(57600);
   Serial.println(F("** START **"));
 
-  s000.attach(4);
-  s120.attach(3);
-  s240.attach(2);
+  // connect to the servos
+  s000.attach(5);
+  s120.attach(4);
+  s240.attach(3);
   
+  // center the servos
   s000.write(90);
   s120.write(90);
   s240.write(90);
   
+  // I use this delay to unplug after the servos are centered.
+  // Makes installing the biceps easier.
   Serial.print(F("5..."));
   delay(1000);
   Serial.print(F("4..."));
@@ -49,12 +63,17 @@ void setup() {
 
 
 //------------------------------------------------------------------------------
+// runs over and over.  if loop() finishes then the board calls it again.
+// globals do not get reset if loop() ends.
+//------------------------------------------------------------------------------
 void loop() {
   float t = (float)millis()*0.001f;
   
-  float x000 = 90 + DIF_ANGLE + sin( t*0.500               ) * TOTAL_ANGLE;
-  float x120 = 90 + DIF_ANGLE + sin( t*0.250 +PI*1.0f/3.0f ) * TOTAL_ANGLE;
-  float x240 = 90 + DIF_ANGLE + sin( t*0.125 +PI*2.0f/3.0f ) * TOTAL_ANGLE;
+  // sine curve from MIN_ANGLE to MAX_ANGLE over time.
+  float speedup=2;
+  float x000 = 90 + DIF_ANGLE + sin( t*speedup               ) * TOTAL_ANGLE;
+  float x120 = 90 + DIF_ANGLE + sin( t*speedup +PI*2.0f/3.0f ) * TOTAL_ANGLE;
+  float x240 = 90 + DIF_ANGLE + sin( t*speedup +PI*4.0f/3.0f ) * TOTAL_ANGLE;
 
   Serial.print(x000);
   Serial.print(F("\t"));
@@ -63,9 +82,10 @@ void loop() {
   Serial.print(x240);
   Serial.print(F("\n"));
   
-  s000.writeMicroseconds((int)((x000 - 90.0)*500.0/45.0+1500.0));
-  s120.writeMicroseconds((int)((x120 - 90.0)*500.0/45.0+1500.0));
-  s240.writeMicroseconds((int)((x240 - 90.0)*500.0/45.0+1500.0));
+  // convert range 180-0 to 2000-1000
+  s000.writeMicroseconds((int)( (x000 * 1000.0/180.0 ) + 1000.0));
+  s120.writeMicroseconds((int)( (x120 * 1000.0/180.0 ) + 1000.0));
+  s240.writeMicroseconds((int)( (x240 * 1000.0/180.0 ) + 1000.0));
   
   delay(20);
 }
